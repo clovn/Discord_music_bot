362f934d22a59ada65a66a9627b49399415bd848
clovn <haliullin101@mail.ru>
2022-11-03 19:55:13 +0300

    OOP

--- bot.py
+++ bot.py
@@ -1,100 +1,107 @@
 import discord
-from discord.ext import tasks
-from song import searcher, update_audio_url
 from discord.ext import commands
+from discord.ext import tasks
 from discord.utils import get
+
 from CONST import TOKEN
+from song import Song
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-music_list = []
+queue = []
 
-
-YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'False'}
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'False'}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
+
 @bot.command()
-async def play(ctx, *, arg):
-    global vc
-    global info_list
+async def play(ctx, *, query):
+    song = Song(query, ctx.message.channel.id, ctx.message.author.voice.channel,
+                get(bot.voice_clients, guild=ctx.guild), ctx.message.author, ctx.message.author.avatar)
 
-    info_list = searcher(arg) # [audio_url, title, image_url, video_url, chanel_id]
-    info_list.append(ctx.message.channel.id)
-    info_list.append('')
+    if song.client is None:
+        try:
+            song.client = await song.voice_channel.connect()
+        except Exception as e:
+            await ctx.send('Бот подключен к другому каналу')
+            print(e, 'string 30')
+
     try:
-        voice_channel = ctx.message.author.voice.channel
-        info_list[5] = get(bot.voice_clients, guild=ctx.guild)
-
-        if info_list[5] and info_list[5].is_connected():
-            await info_list[5].move_to(voice_channel)
+        if song.client.is_playing():
+            await ctx.send(embed=song.create_message('add_to_queue', len(queue)))
         else:
-            info_list[5] = await voice_channel.connect()
+            await ctx.send(embed=song.create_message('playing', len(queue)))
 
-        if info_list[5].is_playing():
-            music_list.append(info_list)
-            embed = discord.Embed(title=f'Position in queue #{int(music_list.index(info_list)+1)}', description=f'[{info_list[1]}]({info_list[3]})',color=discord.Color.blue())
-            embed.set_thumbnail(url=info_list[2])
-            # embed.set_footer(text=f'Added by {ctx.message.author}', icon_url=ctx.message.author.avatar.url)
-            await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(title=':musical_note: Now playing', description=f'[{info_list[1]}]({info_list[3]})', color=discord.Color.blue())
-            embed.set_thumbnail(url=info_list[2])
-            # embed.set_footer(text=f'Added by {ctx.message.author}', icon_url=ctx.message.author.avatar.url)
-            await ctx.send(embed=embed)
-
-
-            info_list[5].play(discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=info_list[0], **FFMPEG_OPTIONS))
-
-
-
-    except EOFError as e:
-        print(e)
-
-@tasks.loop(seconds=5.0)
-async def main_loop():
-    print(len(music_list))
-    try:
-        if info_list[5].is_connected() and not info_list[5].is_playing() and len(music_list) >= 1:
-
-                embed = discord.Embed(title='Now playing', description=f'[{music_list[0][1]}]({music_list[0][3]})',
-                                      color=discord.Color.blue())
-                embed.set_thumbnail(url=music_list[0][2])
-                # embed.set_footer(text=f'Added by {user.message.author}', icon_url=user.message.author.avatar.url)
-                await bot.get_channel(info_list[4]).send(embed=embed)
-                # music_list[0][0] = update_audio_url(music_list[0][3])
-                info_list[5].play(discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=music_list[0][0], **FFMPEG_OPTIONS))
-                music_list.remove(music_list[0])
+        queue.append(song)
     except Exception as e:
-        print(e)
+        print(e, 'string 38')
+
+    # info_list.append(ctx.message.channel.id)
+    # info_list.append('')
+    # try:
+    #     voice_channel = ctx.message.author.voice.channel
+    #     info_list[5] = get(bot.voice_clients, guild=ctx.guild)
+    #
+    #     if info_list[5] and info_list[5].is_connected():
+    #         await info_list[5].move_to(voice_channel)
+    #     else:
+    #         info_list[5] = await voice_channel.connect()
+    #
+    #     if info_list[5].is_playing():
+    #         queue.append(info_list)
+    #         embed = discord.Embed(title=f'Position in queue #{int(queue.index(info_list)+1)}', description=f'[{info_list[1]}]({info_list[3]})', color=discord.Color.blue())
+    #         embed.set_thumbnail(url=info_list[2])
+    #         # embed.set_footer(text=f'Added by {ctx.message.author}', icon_url=ctx.message.author.avatar.url)
+    #         await ctx.send(embed=embed)
+    #     else:
+    #         await ctx.send(embed=create_message('playing', info_list[1], info_list[2], info_list[3], ctx.message.author, ctx.message.author.avatar))
+    #         info_list[5].play(discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=info_list[0], **FFMPEG_OPTIONS))
+    #
+    # except EOFError as e:
+    #     print(e)
+
+
+@tasks.loop(seconds=2.0)
+async def main_loop():
+    try:
+        if not queue[0].playing and len(queue) > 0:
+            await bot.get_channel(queue[0].text_channel).send(embed=queue[0].create_message('playing', len(queue)))
+            queue[0].client.play(discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=queue[0].track, **FFMPEG_OPTIONS), after=lambda e: queue.remove(queue[0]))
+            queue[0].playing = True
+
+    except Exception as e:
+        print(e, 'string 74')
+
+
 @bot.listen()
 async def on_ready():
     main_loop.start()
-# @main_loop.before_loop # Это необходимо, чтобы ваш таск ждал, пока бот полностью запустится.
-# async def yourname2(): # Назвать эту функцию можно тоже как угодно.
-#     await bot.wait_until_ready()
 
-@bot.command()
-async def pause(ctx):
-    try:
-        info_list[5].pause()
-    except Exception as e:
-        print(e)
 
-@bot.command()
-async def resume(ctx):
-    try:
-        info_list[5].resume()
-    except Exception as e:
-        print(e)
+# @bot.command()
+# async def pause(ctx):
+#     try:
+#         info_list[5].pause()
+#     except Exception as e:
+#         print(e)
+
+
+# @bot.command()
+# async def resume(ctx):
+#     try:
+#         info_list[5].resume()
+#     except Exception as e:
+#         print(e)
+
 
 @bot.command()
 async def ql(ctx):
     try:
-        if len(music_list) >= 1:
+        if len(queue) > 0:
             embed = discord.Embed(title=f'Play next...', description=None, color=discord.Color.green())
-            for i in range(1, len(music_list)+1):
-                embed.add_field(name=f'#{i}', value=f'{music_list[i-1][1]}', inline=False)
+            for i in range(1, len(queue) + 1):
+                embed.add_field(name=f'#{i}', value=f'{queue[i - 1][1]}', inline=False)
             await ctx.send(embed=embed)
         else:
             embed = discord.Embed(title=f':toolbox: Error', description=f'There is nothing In queue',
@@ -103,31 +110,34 @@ async def ql(ctx):
     except Exception as e:
         print(e)
 
-@bot.command()
-async def end(ctx):
-    try:
-        info_list[5].stop()
-    except EOFError as e:
-        print(e)
-        await ctx.send("ty eblan")
 
-@bot.command()
-async def skip(ctx):
-    try:
-        if info_list[5].is_playing() and len(music_list) >= 1:
-            info_list[5].stop()
-            await ctx.send('music skipped')
-        else:
-            embed = discord.Embed(title=f':toolbox: Error', description=f'There is nothing to skip',
-                                  color=discord.Color.red())
-            await ctx.send(embed=embed)
-    except Exception as e:
-        print(e)
+# @bot.command()
+# async def end(ctx):
+#     try:
+#         info_list[5].stop()
+#     except EOFError as e:
+#         print(e)
+#         await ctx.send("ty eblan")
+#
+#
+# @bot.command()
+# async def skip(ctx):
+#     try:
+#         if info_list[5].is_playing() and len(queue) >= 1:
+#             info_list[5].stop()
+#             await ctx.send('music skipped')
+#         else:
+#             embed = discord.Embed(title=f':toolbox: Error', description=f'There is nothing to skip',
+#                                   color=discord.Color.red())
+#             await ctx.send(embed=embed)
+#     except Exception as e:
+#         print(e)
+
 
 # @bot.command()
 # async def skip():
 #     try:
-#         await vc.skip()
+#         await info_list[5].skip()
 #     except Exception as e:
 #         print(e)
 
--- song.py
+++ song.py
@@ -1,32 +1,49 @@
+import discord
 from youtube_dl import YoutubeDL
+
 from youtube_search import YoutubeSearch
 
-YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'False'}
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'False'}
 
-def searcher(meta):
-    res = YoutubeSearch(meta, max_results=1).to_dict()
-    try:
-        with YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(res[0]['id'], download=False)
-            audio_url = info['formats'][0]['url']
-            title = info['title']
-            chanel_url = info['channel_url']
-            image_url = info['thumbnails'][0]['url']
-            video_url = info['webpage_url']
 
-    except:
-        with YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(res[0]['id'], download=False)
-            audio_url = info['formats'][0]['url']
-            title = info['title']
-            chanel_url = info['channel_url']
-            image_url = info['thumbnails'][0]['url']
-            video_url = info['webpage_url']
-    return [audio_url, title, image_url, video_url]
+class Song(object):
 
-def update_audio_url(res):
-    with YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(res, download=False)
-        audio_url = info['formats'][0]['url']
-        return audio_url
+    playing = False
 
+    def __init__(self, meta, text_channel, voice_channel, client, author, avatar):
+        try:
+            res = YoutubeSearch(meta, max_results=1).to_dict()
+            with YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(res[0]['id'], download=False)
+                self.track = info['formats'][0]['url']
+                self.title = info['title']
+                self.image_url = info['thumbnails'][0]['url']
+                self.video_url = info['webpage_url']
+        except Exception as e:
+            print(e)
+
+        self.text_channel = text_channel
+        self.voice_channel = voice_channel
+        self.client = client
+        self.author = author
+        self.avatar = None if avatar is None else avatar.url
+
+    def create_message(self, type_, index):
+        if type_ == 'playing':
+            embed = discord.Embed(title=':musical_note: Now playing', description=f'[{self.title}]({self.video_url})',
+                                  color=discord.Color.blue())
+            embed.set_thumbnail(url=self.image_url)
+            embed.set_footer(text=f'Added by {self.author}', icon_url=self.avatar)
+            return embed
+
+        elif type_ == 'add_to_queue':
+            embed = discord.Embed(title=f'Position in queue #{index}',
+                                  description=f'[{self.title}]({self.video_url})', color=discord.Color.blue())
+            embed.set_thumbnail(url=self.image_url)
+            embed.set_footer(text=f'Added by {self.author}', icon_url=self.avatar)
+            return embed
+
+        elif type_ == 'queue':
+            pass
+        elif type_ == 'error':
+            pass
